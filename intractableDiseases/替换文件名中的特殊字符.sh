7a9c3452a81a6428514207ab261c461aed1050c2
cuncun-edg <cuncunye@qq.com>
2024-10-24 15:57:17 +0800

    添加hover动画效果

--- intractableDiseases/替换文件名中的特殊字符.sh
+++ intractableDiseases/替换文件名中的特殊字符.sh
@@ -1,8 +1,8 @@
 # ls -b 显示文件名的详细信息，包括隐藏的特殊字符
 # 使用 tr 命令删除所有不可见的特殊字符
 
-for file in zhongwu*; do
-  newfile=$(echo "$file" | tr -d '\360\237\215\203')
+for file in *; do
+  newfile=$(echo "$file" | tr -d '\351\222')
   mv "$file" "$newfile"
 done
 
